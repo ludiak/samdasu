ec500696085b1490dea708f0abdf123cb4cad0d6
ludia <ludiak7@gmail.com>
2021-10-28 16:18:11 +0900

    proxy

--- samdasu/src/main/resources/front/src/App.js
+++ samdasu/src/main/resources/front/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState("hi");
+
+  useEffect(()=> {
+    axios.get("/time")
+    .then(response => {
+      console.log(response);
+      setMessage(response.data);
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +27,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <p>hahaha {message}</p>
       </header>
     </div>
   );
